be85df9f7b8599d92fe656a3f9144ec027490e11
Kimi Maulana <kimi93.com@gmail.com>
2025-03-16 11:29:23 +0700

    update name

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
     <section id="hero" className="container mx-auto px-8">
       <div className="flex flex-col lg:flex-row gap-14 items-center justify-between mt-[80px]">
         <div className="order-2 lg:order-1 text-center lg:text-left mt-16 lg:mt-0">
-          <h3 className="text-xl lg:text-2xl font-medium text-black">👋 Hi, I’m Alex Carter</h3>
+          <h3 className="text-xl lg:text-2xl font-medium text-black">👋 Hi, I’m Kimi Maulana</h3>
           <h1 className="w-full lg:w-[480px] text-4xl lg:text-5xl font-bold leading-[50px] lg:leading-[60px] mt-3 bg-gradient-primary bg-clip-text text-transparent">Building Scalable & User Centric Web Apps</h1>
 
           <p className="w-full lg:w-[500px] text-sm lg:text-base mt-4">
