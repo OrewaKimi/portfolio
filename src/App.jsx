1ef52dc153d5c472961632285538afecc95c6c8e
Kimi Maulana <kimi93.com@gmail.com>
2025-03-16 11:49:27 +0700

    update page for footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import TechnicalProficiency from './sections/TechnicalProfiency'
 import AboutMe from './sections/AboutMe'
 import MyProjects from './sections/MyProjects'
 import ContactMe from './sections/ContactMe'
+import Footer from './sections/Footer'
 
 const App = () => {
   return (
-    <div className='mb-80'>
+    <div className='flex flex-col min-h-screen'>
       <Navbar />
       <Hero />
       <TechnicalProficiency />
       <AboutMe />
       <MyProjects />
       <ContactMe/>
+      <Footer className='mt-auto'/>
     </div>
   )
 }
